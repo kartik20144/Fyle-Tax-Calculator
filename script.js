a7ca5dc8d9d810566e19f494f0d22126a21f00aa
Kartik <kartikagarwal20144@gmail.com>
2024-04-10 23:22:10 +0530

    age group error fiexed

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ const dialog = document.getElementById("favDialog");
 
 const numberField = document.querySelectorAll(".numberField");
 
+const selectField = document.querySelector("#select-age");
+
+selectField.addEventListener("input",()=>{
+    selectField.nextElementSibling.style.display=  "none";
+})
+
 numberField.forEach(field => {
 
     const errorIcon = field.nextElementSibling;
@@ -52,18 +58,31 @@ function isValidNumber(value){
 // })
 
 form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
     const grossIncome = document.getElementById("gross-income").value;
 const extraIncome = document.getElementById("extra-income").value;
 const deductions = document.getElementById("deduction-income").value;
 const submit = document.querySelector("#submit");
-const rate = document.querySelector("#select-age").value;
+const selectField = document.querySelector("#select-age");
+
+const rate = selectField.value;
+
+const errorIcon = selectField.nextElementSibling;
+    const tooltip = errorIcon.nextElementSibling;
+
+if(rate===''){
+    errorIcon.style.display = 'inline-block'; // Show error icon
+    // tooltip.style.display = 'block'; // Show tooltip
+    return;
+} else {
+    errorIcon.style.display = 'none'; // Hide error icon
+    tooltip.style.display = 'none'; // Hide tooltip
+}
 
 
 
-
-
-
-    e.preventDefault();
+    
     const netIncome = Number(grossIncome) + Number(extraIncome) - Number(deductions);
     
     let tax = 0;
